f551cbefcf761344fd739bf5d2c59ea5f2324332
westhide.dev <westhide.dev@gmail.com>
2024-11-15 21:34:38 +0800

    fix: pco_c

--- pco_c/include/cpcodec_generated.h
+++ pco_c/include/cpcodec_generated.h
@@ -1,3 +1,4 @@
+
 typedef enum PcoError {
   PcoSuccess,
   PcoInvalidType,
@@ -8,18 +9,16 @@ typedef enum PcoError {
 typedef struct PcoFfiVec {
   const void *ptr;
   size_t len;
-  const void *raw_box;
+  size_t cap;
 } PcoFfiVec;
 
-enum PcoError pco_simpler_compress(const void *nums,
-                                   size_t len,
-                                   unsigned char dtype,
-                                   unsigned int level,
-                                   struct PcoFfiVec *dst);
+enum PcoError pco_simple_compress(const void *nums, size_t len,
+                                  unsigned char dtype, unsigned int level,
+                                  struct PcoFfiVec *dst);
 
-enum PcoError pco_simple_decompress(const void *compressed,
-                                    size_t len,
-                                    unsigned char dtype,
-                                    struct PcoFfiVec *dst);
+enum PcoError pco_simple_decompress(const void *compressed, size_t len,
+                                    unsigned char dtype, struct PcoFfiVec *dst);
 
-enum PcoError pco_free_pcovec(struct PcoFfiVec *ffi_vec);
+enum PcoError pco_free_cvec(struct PcoFfiVec *ffi_vec);
+
+enum PcoError pco_free_dvec(struct PcoFfiVec *ffi_vec, unsigned char dtype);
--- pco_c/test/run_test.sh
+++ pco_c/test/run_test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-set -e
+set -ex
 
-cd "${0%/*}"
+CUR_DIR=$(realpath "$(dirname "${BASH_SOURCE:-$0}")")
+
+cd "${CUR_DIR}"
+
+cargo build
 gcc -g test_cpcodec.c -o test_cpcodec -L../../target/debug -lcpcodec -Wl,-rpath,../../target/debug
 ./test_cpcodec
 rm test_cpcodec
